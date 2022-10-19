9c4592c6a06a6960c3d340bb95c7ac12b242b0f3
AsiyeGokalp <107327598+AsiyeGokalp@users.noreply.github.com>
2022-10-19 13:54:29 +0200

    Update app.js

--- homework/hackyourtemperature/app.js
+++ homework/hackyourtemperature/app.js
@@ -6,21 +6,28 @@ import express from "express"
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get("/", (req, res) => {
   res.setHeader("Content-type", "text/plain")
   res.send("hello from backend to frontend!")
 })
 
 app.post("/weather", async (req, res) => {
-   const city = req.body.cityname
+  const city = req.body.cityname
   if (!city) {
     return res.status(400).json({ msg: "City not provided!" })
   }
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${keys}`
-  )
-  const data = await response.json()
-  res.json({msg:`${data.name} is today ${data.main.temp}°C`})
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${keys}`
+    )
+    const data = await response.json()
+    res.json({ msg: `${data.name} is today ${data.main.temp}°C` })
+  } catch (error) {
+    throw error
+  }
 })
 
-export default app;
+export default app
