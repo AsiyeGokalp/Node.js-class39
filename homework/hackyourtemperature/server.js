dfda1da7fe20830c811fa950af0037459c77a0be
AsiyeGokalp <107327598+AsiyeGokalp@users.noreply.github.com>
2022-10-19 13:55:18 +0200

    Update server.js

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -2,9 +2,7 @@
 import app from "./app.js"
 import express from "express"
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+
